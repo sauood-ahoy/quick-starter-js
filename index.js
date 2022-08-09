1cb6582632c1a89bdf40708e41b239caa1e57f2d
sauood-ahoy <m.meeran@theahoyapp.com>
2022-08-09 19:53:44 +0400

    Add Showing map

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-// Import stylesheets
-import './style.css';
+const mapElement = document.querySelector('#mapContainer');
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+let platform;
+let defaultLayers;
+let map;
+
+async function initializeMap() {
+  platform = await AhoyMapView.service().Platform(SUBSCRIPTION_KEY);
+  defaultLayers = platform.createDefaultLayers();
+  map = AhoyMapView.Map(mapElement, defaultLayers.vector.normal.map, {
+    zoom: 4,
+    center: {
+      lat: 34,
+      lng: 41,
+    },
+  });
+
+  // default interactions for pan/zoom
+  const mapEvents = AhoyMapView.mapevents().MapEvents(map);
+  const behavior = AhoyMapView.mapevents().Behavior(mapEvents);
+
+  // Create the default UI components
+  const ui = AhoyMapView.ui().UI.createDefault(map, defaultLayers);
+}
+
+initializeMap();
